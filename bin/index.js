7d168142ef6a2c28b16c64be447a6061731cc7d6
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2022-11-15 14:02:52 -0800

    markdownlint taming

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
-console.log( "Welcome to Feeder-CLI!" );
+
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
